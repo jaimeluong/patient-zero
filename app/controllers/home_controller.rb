b7d089a839eab216d997d8bf3d90a8c7bd3023e0
Jaime Luong <jaime.luong@gmail.com>
2023-02-01 20:17:39 -0600

    Show analytics on homepage if signed in, generic message if not

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
     def index
+        if user_signed_in?
+            @provider_count = Provider.all.count
+            @patient_count = Patient.all.count
+            @appointment_count = Appointment.all.count
+
+            respond_to do |format|
+                format.html
+            end
+        else
+            render "splash"
+        end
     end
 end
